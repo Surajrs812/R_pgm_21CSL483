25dd56b57080c8124ee1f12ed78c8d41863cc2dd
Suraj R S <118114735+Surajrs812@users.noreply.github.com>
2023-09-10 10:00:25 +0530

    Add files via upload

---
+++ pgm_01.R
@@ -0,0 +1,15 @@
+
+# Program 1: Write an R program to print fibonacci series upto given number n
+
+fibonacci <- function(n){
+  if(n==1)
+    return(0)
+  else if(n==2)
+    return(1)
+  else 
+    return(fibonacci(n-1)+fibonacci(n-2))
+}
+
+limit <- as.numeric(readline('Enter the series range: '))
+for(i in 0:limit+1)
+  print(paste(fibonacci(i)))
